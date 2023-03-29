46098a41bfad284859fe38bd4c0671c7b28be95d
sabrina-setu <sabrinayeasminshoshi@gmail.com>
2023-03-29 10:09:37 +0600

    sign in is working

--- JWT.Api/Controllers/Account/AuthController.cs
+++ JWT.Api/Controllers/Account/AuthController.cs
@@ -33,4 +33,13 @@ public class AuthController : ControllerBase
         if (result.Succeeded) return Ok(token);
         return BadRequest();
     }
+    [HttpPost("SignIn")]
+    public async Task<IActionResult> SignIn(SignInRequestModel signInRequest)
+    {
+        var user = await userManager.FindByEmailAsync(signInRequest.Email);
+        if (user == null || !await userManager.CheckPasswordAsync(user, signInRequest.Password)) 
+            return BadRequest("Email or Password is not correct!");
+        var token = tokenGenerator.GenerateToken(user);
+        return Ok(token);
+    }
 }
---
+++ JWT.Api/RequestModels/Account/SignInRequestModel.cs
@@ -0,0 +1,7 @@
+﻿namespace JWT.Api.RequestModels.Account;
+
+public class SignInRequestModel
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
